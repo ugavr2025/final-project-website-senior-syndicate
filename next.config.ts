9e102d544092d6d0c5f6d696b9be0fb8562968f7
Kelsey Sterner <132179416+KelseySt@users.noreply.github.com>
2025-11-04 13:50:26 -0500

    Update next.config.ts with new configuration options

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
+const nextConfig = {
+  basePath: "/2048-in-react",
+  output: "export",
+  reactStrictMode: true,
 };
+
+module.exports = nextConfig;
 
 export default nextConfig;
