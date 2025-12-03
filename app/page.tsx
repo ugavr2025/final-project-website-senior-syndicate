16e26893ee2c72b75222dc1da9e8caca4158f042
Marra Young <131184358+marrayoung@users.noreply.github.com>
2025-12-02 22:57:34 -0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -34,10 +34,14 @@ export default function Home() {
       <div className="border-l-4 border-blue-500 pl-6">
         <h2 className="text-3xl font-semibold tracking-tight">Project Description</h2>
         <p className="mt-3 text-gray-600 text-lg leading-relaxed">
-          This project is a virtual Fabrication Lab and Machine Shop simulation designed to provide users with an interactive, 
-          immersive learning experience. Users can explore and operate a variety of machines typically found in a fab lab, gaining hands-on experience in a safe virtual environment. 
-          The simulation aims to enhance understanding of manufacturing processes, machine operations, 
-          and fabrication techniques while fostering engagement and practical learning without the risks or limitations of a physical lab.
+          This project is a virtual Fabrication Lab and Machine Shop simulation designed to provide engineering students with an interactive, 
+          immersive training experience. Within the simulation, users can navigate a realistic fab lab environment, 
+          interact with common tools and learn essential safety procedures before ever stepping into a physical workspace. A central focus of the platform is the 
+          SawStop table saw and its core safety features as it is used in the UGA Fabrication Laboratory. The simulation models the machine's most 
+          important real-world behaviors, including required key card access, immediate blade stoppage on skin contact, 
+          and activation only after meeting required lab conditions (such as proper attire, including safety goggles and earplugs). By accurately replicating these systems, 
+          The simulation allows students to practice proper machine setup, operation, and emergency response in a controlled digital environment without the risks, scheduling limitations, 
+          or personnel demands associated with in-person training.
           </p>
       </div>
     </section>
