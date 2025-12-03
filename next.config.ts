4cb2913f83c9f0ff57325b2ed7bfb3c70411cd6c
Marra Young <131184358+marrayoung@users.noreply.github.com>
2025-12-03 00:55:01 -0500

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,14 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   output: 'export',
-  //images: { unoptimized: true },
-  //basePath: "/final-project-website-senior-syndicate",
-  //assetPrefix: "/final-project-website-senior-syndicate/"
+
+  images: {
+    unoptimized: true,
+  },
+
+  // If you later deploy on GitHub Pages, uncomment these:
+  // basePath: "/final-project-website-senior-syndicate",
+  // assetPrefix: "/final-project-website-senior-syndicate/",
 };
 
 export default nextConfig;
